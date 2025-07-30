b12a3ee54ebd7b9971da3886c0359211a67c9b59
biswajit dey <bdey862002@gmail.com>
2025-07-30 22:20:33 +0530

    update the live_link field in certifications and my_resume model

--- certifications/models.py
+++ certifications/models.py
@@ -9,8 +9,8 @@ class Certification(models.Model):
     # image = models.ImageField(upload_to='certificates/images/')  # For certificate thumbnail or badge
     image = CloudinaryField('image')
 
-    # live_link = models.FileField(upload_to='certificates/pdfs/')  # For storing PDF certificate files
-    live_link = CloudinaryField('raw')  # For PDF file,  raw means non-image file (PDF, DOCX, etc.)
+    live_link = models.FileField(upload_to='certificates/pdfs/')  # For storing PDF certificate files
+    # live_link = CloudinaryField('raw')  # For PDF file,  raw means non-image file (PDF, DOCX, etc.)
 
     organization_name = models.CharField(max_length=200)
     issue_date = models.DateField()

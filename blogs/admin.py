e5fc968c2bd136177e7c26612917c7ae3666e040
biswajit dey <bdey862002@gmail.com>
2025-07-30 19:37:33 +0530

    update the blog admin file for clickable theimage link in admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from .models import Blogs
+from django.utils.html import format_html
 
 # Register your models here.
 
 @admin.register(Blogs)
 class BlogsAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'description', 'technology', 'image', 'github_link', 'start_date', 'end_date', 'created_at', 'updated_at']
+
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html(f"<a href='{obj.image.url}' target='_blank'>{obj.image.url}</a>")
+        return "-"
+    
+    image_tag.short_description = 'Image'
 
 

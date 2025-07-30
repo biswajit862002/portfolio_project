f1e8b6755fda6919f28f3b2d65cd87b039fec1c5
biswajit dey <bdey862002@gmail.com>
2025-07-30 19:19:54 +0530

    update the blogs model image field

--- blogs/models.py
+++ blogs/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from datetime import timedelta
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Blogs(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     technology = models.CharField(max_length=300)
-    image = models.ImageField(upload_to='blogs_images/')
+    # image = models.ImageField(upload_to='blogs_images/')
+    image = CloudinaryField('image')
     github_link = models.URLField(blank=True, null=True)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)

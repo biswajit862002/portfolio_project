4dc12523b181205374b57aee0f80fec3c1160424
biswajit dey <bdey862002@gmail.com>
2025-07-30 19:43:06 +0530

    update the blogs admin file no changes

--- blogs/admin.py
+++ blogs/admin.py
@@ -8,11 +8,5 @@ from django.utils.html import format_html
 class BlogsAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'description', 'technology', 'image', 'github_link', 'start_date', 'end_date', 'created_at', 'updated_at']
 
-    def image_tag(self, obj):
-        if obj.image:
-            return format_html(f"<a href='{obj.image.url}' target='_blank'>{obj.image.url}</a>")
-        return "-"
-    
-    image_tag.short_description = 'Image'
 
 
